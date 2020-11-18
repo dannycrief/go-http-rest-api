57422cfd0cd40857ccb1ef24cdbc91d647481dd6
Stepan Kozurak <step.kozbvb@gmail.com>
2020-11-18 21:12:29 +0100

    Added db configuration and db testing

---
+++ interanal/app/store/store_test.go
@@ -0,0 +1,19 @@
+package store_test
+
+import (
+	"os"
+	"testing"
+)
+
+var (
+	databaseURL string
+)
+
+func TestMain(m *testing.M) {
+	databaseURL = os.Getenv("database_url")
+	if databaseURL == "" {
+		databaseURL = "postgres://postgres:211217ns@localhost/restapi_test?sslmode=disable"
+	}
+
+	os.Exit(m.Run())
+}
